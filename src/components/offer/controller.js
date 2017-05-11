3681dd44662497c074dd843b5f4bf2ef931569be
marcelorl <marcelo_rl2@hotmail.com>
2017-05-11 03:55:37 -0300

    offers crud

--- src/components/offer/controller.js
+++ src/components/offer/controller.js
@@ -4,19 +4,35 @@ const Model = require('./model');
 const router = express.Router();
 
 router.get('/offers', (req, res) => {
-  return Model.find();
+  Model.find()
+    .then(offers => res.send(offers));
 });
 
 router.post('/offers', (req, res) => {
-  return Model.find();
+  new Model(req.body).save(err => {
+    if(err) {
+      return res.send(err);
+    }
+    return res.send('Success');
+  });
 });
 
 router.put('/offers/:id', (req, res) => {
-  return Model.find();
+  Model.update({_id: req.params.id}, req.body, err => {
+    if(err) {
+      return res.send(err);
+    }
+    return res.send('Success');
+  });
 });
 
 router.delete('/offers/:id', (req, res) => {
-  return Model.find();
+  Model.remove({_id: req.params.id}, err => {
+    if(err) {
+      return res.send(err);
+    }
+    return res.send('Success');
+  });
 });
 
 module.exports = router;
